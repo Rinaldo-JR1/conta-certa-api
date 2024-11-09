ae071eaa964f7b537419acd81be9a387a9320aae
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-11-08 21:19:11 -0300

    Delete transaction

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -4,15 +4,8 @@ import { VerifyTokenMiddleware } from "../middlewares/verifyToken.middleware";
 
 const transactionRoutes = Router();
 const transactionController = new TransactionController();
-transactionRoutes.get(
-  "/my-transaction/:monthRef",
-  VerifyTokenMiddleware.verifyToken,
-  transactionController.getMyTransactions
-);
-transactionRoutes.post(
-  "/new",
-  VerifyTokenMiddleware.verifyToken,
-  transactionController.createTransaction
-);
+transactionRoutes.get("/my-transaction/:monthRef", VerifyTokenMiddleware.verifyToken, transactionController.getMyTransactions);
+transactionRoutes.post("/new", VerifyTokenMiddleware.verifyToken, transactionController.createTransaction);
+transactionRoutes.delete("/delete", VerifyTokenMiddleware.verifyToken, transactionController.deleteTransaction);
 
 export { transactionRoutes };
