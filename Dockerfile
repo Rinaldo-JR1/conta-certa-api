f92b1af75de46641617304dcb16980850fd146f1
Davi Castro <davialmeida47jd@gmail.com>
2024-11-10 01:50:43 -0300

    deploy: updated deploy

--- Dockerfile
+++ Dockerfile
@@ -9,15 +9,6 @@ RUN npm install
 
 COPY . .
 
-ARG DATABASE_URL
-
-RUN npx prisma generate
-
-RUN npx prisma migrate deploy
-
-RUN npx tsc src/seeder.ts
-
-RUN node src/seeder.js
 
 EXPOSE 4052
 
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$NODE_ENV" == "production" ] ; then
-  npm run start:watch
+  npm run prisma-build && npm run start:watch
 else
-  npm run start:dev
+  npm run prisma-build && npm run start:dev
 fi
